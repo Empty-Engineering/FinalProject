4a77c6686464d8d1d9563e0f746da9d3685fb9c1
Empty-Engineering <lukasmrobin@gmail.com>
2021-09-28 14:54:03 -0400

    Added areas

--- Major.py
+++ Major.py
@@ -1,7 +1,18 @@
+
+
 class Fields:
-    def __init__(self, stem: object, business: object, medicine: object, law: object, social_sciences: object):
-        self.stem = stem;
-        self.business = business; 
-        self.medicine = medicine;
-        self.law = law;
-        self.social_sciences = social_sciences;
+    def __init__(self, field: str, specialty: str):
+        self.field = field;
+        self.specialty = specialty;
+
+    def get_field(self):
+        return self.field;
+
+    def set_field(self, field_name: str):
+         setattr(self.field, 'field', field_name);
+
+def main():
+    newMajor = Fields('stem', 'engineering');
+    newMajor.set_field('law');
+    print(newMajor.field);
+main()
