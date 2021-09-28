89f5763337e97b9037e0f60c48e74a5e34835c4a
Empty-Engineering <lukasmrobin@gmail.com>
2021-09-28 12:11:09 -0400

    Initial Commit

---
+++ Major.py
@@ -0,0 +1,7 @@
+class Fields:
+    def __init__(self, stem: object, business: object, medicine: object, law: object, social_sciences: object):
+        self.stem = stem;
+        self.business = business; 
+        self.medicine = medicine;
+        self.law = law;
+        self.social_sciences = social_sciences;
